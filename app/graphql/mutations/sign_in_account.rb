a79591b013a71caa216b05723fc06a832abf6d21
nozomi-iida <iida19990106@gmail.com>
2021-03-09 16:33:48 +0900

    feat: sign in mutationを実装

---
+++ app/graphql/mutations/sign_in_account.rb
@@ -0,0 +1,19 @@
+module Mutations
+  class SignInAccount < BaseMutation
+    field :account, Types::AccountType, null: false
+    field :token, String, null: false
+
+    argument :email, String, required: true
+    argument :password, String, required: true
+
+    def resolve(email, password)
+      account = Account.find_by!(email: email)
+      fail Exceptions::UnauthorizedError unless account.authenticate(password)
+
+      {
+        account: account,
+        token: account.jwt
+      }
+    end
+  end
+end
--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 FactoryBot.define do
   factory :account do
+    sequence(:username) { |n| "#{n}太郎" }
+    sequence(:email) { |n| "test_#{n}@example.com" }
+    password { "password" }
   end
 end
